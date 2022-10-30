24d5acbc7c187760952333e040dafa2bfc04f484
huangchunwu <951123033@qq.com>
2022-10-30 21:44:50 +0800

    feat: 修改并发工具类

--- j-best-common/j-best-cache/src/main/java/com/j/best/common/cache/config/RedisUtil.java
+++ j-best-common/j-best-cache/src/main/java/com/j/best/common/cache/config/RedisUtil.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -23,5 +24,26 @@ public class RedisUtil {
     public void set(String key, Object val,Long timeout,TimeUnit timeUnit) {
         redisTemplate.opsForValue().set(key,val,timeout, timeUnit);
     }
+
+    public Long lLeftPush(String key,String value){
+        return redisTemplate.opsForList().leftPush(key,value);
+    }
+
+    public Long lLeftPushAll(String key,String ... value) {
+        return redisTemplate.opsForList().leftPushAll(key,value);
+    }
+
+    public Long lLeftPushAll(String key, Collection<String> value) {
+        return redisTemplate.opsForList().leftPushAll(key,value);
+    }
+
+    public String lRightPop(String key) {
+        return (String)redisTemplate.opsForList().rightPop(key);
+    }
+
+    public String lRightPop(String key, long timeout, TimeUnit timeUnit) {
+        return (String) redisTemplate.opsForList().rightPop(key,timeout,timeUnit);
+    }
+
 
 }
