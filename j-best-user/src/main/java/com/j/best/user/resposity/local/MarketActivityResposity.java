2e4a90c15d00d331458aebeb97b02aa681df687b
huangchunwu <951123033@qq.com>
2022-11-20 19:55:15 +0800

    feat: 活动规则

--- j-best-user/src/main/java/com/j/best/user/resposity/local/MarketActivityResposity.java
+++ j-best-user/src/main/java/com/j/best/user/resposity/local/MarketActivityResposity.java
@@ -4,6 +4,7 @@ import com.j.best.user.infrastructure.mapper.MarketActivityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class MarketActivityResposity {
 
